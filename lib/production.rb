45d9b5e9c22b8876e464fc584ce22164d71ad27b
Scott Sansovich <ssansovich@gmail.com>
2014-08-09 17:47:39 -0400

    disabling aws and stripe

--- lib/production.rb
+++ lib/production.rb
@@ -5,33 +5,33 @@ DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/trd_test")
 
 if ENV['RACK_ENV'] == 'production'
-  set :bucket, ENV['S3_BUCKET_NAME']
-  set :s3_key, ENV['AWS_ACCESS_KEY_ID']
-  set :s3_secret, ENV['AWS_SECRET_ACCESS_KEY']
-  set :session_secret, 'manjusri'
+  #set :bucket, ENV['S3_BUCKET_NAME']
+  #set :s3_key, ENV['AWS_ACCESS_KEY_ID']
+  #set :s3_secret, ENV['AWS_SECRET_ACCESS_KEY']
+  #set :session_secret, 'manjusri'
 
-  AWS::S3::Base.establish_connection!(
-  :access_key_id     => settings.s3_key,
-  :secret_access_key => settings.s3_secret)
+  #AWS::S3::Base.establish_connection!(
+  #:access_key_id     => settings.s3_key,
+  #:secret_access_key => settings.s3_secret)
 
   set :stripe_key, ENV['STRIPE_SECRET_KEY']
 
   DataMapper::Model.raise_on_save_failure = false 
 
-  =begin
-  Pony.options = {
-    :via => :smtp,
-    :via_options => {
-      :address => 'smtp.sendgrid.net',
-      :port => '587',
-      :domain => 'heroku.com',
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  }
-  =end
+  ##
+  #Pony.options = {
+  #  :via => :smtp,
+  #  :via_options => {
+  #    :address => 'smtp.sendgrid.net',
+  #    :port => '587',
+  #    :domain => 'heroku.com',
+  #    :user_name => ENV['SENDGRID_USERNAME'],
+  #    :password => ENV['SENDGRID_PASSWORD'],
+  #    :authentication => :plain,
+  #    :enable_starttls_auto => true
+  #  }
+  #}
+  
 else
   set :session_secret, 'manjusri'
   set :stripe_key, 'sk_test_4YpFppsWInTw4StJOGH60h8v'
